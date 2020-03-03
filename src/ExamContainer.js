cd6f3fd8af15cadeb01177547a138c17e4b234d3
Luke Geneva <lukegeneva@gmail.com>
2020-03-03 17:41:23 -0600

    Enable exam fact iteration
    Accepts answers and switches the displayed fact when the correct
    answer is given.

--- src/ExamContainer.js
+++ src/ExamContainer.js
@@ -3,10 +3,32 @@ import shuffle from 'lodash/shuffle';
 
 function ExamContainer({ facts }) {
   const [factLineup] = React.useState(shuffle(facts));
-  const [factIndex] = React.useState(0);
+  const [factIndex, setFactIndex] = React.useState(0);
+  const [answer, setAnswer] = React.useState('');
   const fact = factLineup[factIndex];
+  const inputRef = React.useRef();
 
-  return <div>{fact.expression}</div>;
+  React.useEffect(() => {
+    inputRef.current.focus();
+  }, [factIndex]);
+
+  React.useEffect(() => {
+    if (answer === fact.answer.toString()) {
+      setFactIndex(factIndex + 1);
+      setAnswer('');
+    }
+  }, [answer, fact, factIndex]);
+
+  const handleAnswerChange = e => setAnswer(e.target.value);
+
+  return (
+    <div>
+      <div>{fact.expression}</div>
+      <div>
+        <input ref={inputRef} type="text" onChange={handleAnswerChange} value={answer} />
+      </div>
+    </div>
+  );
 }
 
 export default ExamContainer;
