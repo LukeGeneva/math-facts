04bb617262f59abf9cf3d2c4067dbd4170beec68
Luke Geneva <lukegeneva@gmail.com>
2020-03-05 20:51:22 -0600

    Improve menu view
    Styled menu for larger buttons and better spacing.

--- src/FactMenu.js
+++ src/FactMenu.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import range from 'lodash/range';
+import './FactMenu.css';
 
 function FactMenu({ onSelect }) {
   return (
-    <div className="menu">
-      <ul className="menu-list">
-        {range(9).map(n => (
-          <li key={n} onClick={() => onSelect([n, n + 1])}>
-            {n}s and {n + 1}s
+    <div className="FactMenu">
+      <div className="menu">
+        <ul className="menu-list">
+          {range(9).map(n => (
+            <li key={n} onClick={() => onSelect([n, n + 1])}>
+              <button className="button is-large is-primary">
+                {n}s and {n + 1}s
+              </button>
+            </li>
+          ))}
+          <li>
+            <button className="button is-large is-primary">Mixed Facts</button>
           </li>
-        ))}
-        <li>Mixed Facts</li>
-      </ul>
+        </ul>
+      </div>
     </div>
   );
 }
